88a33490f21607a2848fb39c842619aaa5ea5edc
Amena Shahid <amenashahid@usf.edu>
2020-01-21 16:46:58 -0500

    commit the change

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace VSDemowithGITHUB
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("heres a change World!");
         }
     }
 }
